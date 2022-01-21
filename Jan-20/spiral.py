ac4d02b570600a1cb21bf56cfee2712ab2279135
Ben <ben.dupey@grandview.edu>
2022-01-20 19:15:31 -0600

    finished spiral.py

--- Jan-20/spiral.py
+++ Jan-20/spiral.py
@@ -2,13 +2,22 @@
 Created on Wednesday, Jan 19 22:25:25 2022
 @author: mrobbins
 """
+#Goal of spiral is to use turtle to draw a spiral shape
 
 from turtle import * 
 
-def spiral():
-  print("Canary")
-  
+def spiral(firststep, angle, gap):
+    """ Move turtle on a spiral path """
+    step = firststep
+    while step > 0:
+        forward (step)
+        left(angle)
+        step -= gap
+        # step -= gap is the same as step = step - gap
+        
 def main():
-  print("Canary main")
+  spiral(100,71,2)
+  spiral(100,71,-5)
+  exitonclick()
   
 main()
